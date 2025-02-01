4d4b5e5a99ab70b5de0b3ca2cb10adce05ece61a
Anton Miroshnyk <miroshniktyta@gmail.com>
2025-02-01 17:16:52 +0100

    Initial Commit

---
+++ tmda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tmda
+//
+//  Created by pc on 01.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
